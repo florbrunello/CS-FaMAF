abf1bfbc0d70e4104c31dbaad23f6f85c76652f7
florbrunello <101515875+florbrunello@users.noreply.github.com>
2022-06-30 22:55:02 -0300

    Add files via upload

---
+++ Segundo/1erCuatrimestre/AyEDII/Laboratorio/Laboratorio1/Ejercicio4/array_helpers.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#ifndef ARRAY_HELPERS
+#define ARRAY_HELPERS
+
+unsigned int array_from_file(int a[], unsigned int max_size,const char *filepath);
+void array_dump(int a[], unsigned int length);
+bool array_is_sorted(int a[], unsigned int length);
+
+#endif
