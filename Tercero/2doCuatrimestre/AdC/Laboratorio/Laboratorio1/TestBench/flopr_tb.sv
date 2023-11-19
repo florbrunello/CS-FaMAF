894a9f67193ea069b04a0c33a07fd067adff4563
Florencia Brunello <florenciabrunello@mi.unc.edu.ar>
2023-11-19 11:26:24 -0300

    Update flopr_tb.sv

--- Tercero/2doCuatrimestre/AdC/Laboratorio/Laboratorio1/TestBench/flopr_tb.sv
+++ Tercero/2doCuatrimestre/AdC/Laboratorio/Laboratorio1/TestBench/flopr_tb.sv
@@ -1,21 +1,20 @@
+// Flopr TestBench
+
 module flopr_tb();
 	parameter N = 64;
 	logic        clk, reset;
 	logic        [N-1:0] d, q, qexpected;
-	//Con 4 bits puedo recorrer los 10+1 numeros (tan grande como quiera)
-	logic [100:0] i, errors;                            // bookkeeping variables 
-	// 10 numeros con 64 bits cada uno
-	logic [128-1:0] testvectors [0:9] = '{ {64'b0000 , 64'b0000},	// array of testvectors
-														{64'b0001 , 64'b0000},  // ver en que indicie esta el arreglo cuando el reset es 0 
-														{64'b0010 , 64'b0000},
-														{64'b0011 , 64'b0000},
-														{64'b0100 , 64'b0000},
-														{64'b0101 , 64'b0101},
-														{64'b0110 , 64'b0110},
-														{64'b0111 , 64'b0111},
-														{64'b1000 , 64'b1000},
-														{64'b1001 , 64'b1001}};
-
+	logic [100:0] i, errors;
+	logic [128-1:0] testvectors [0:9] = '{  {64'b0000 , 64'b0000},
+						{64'b0001 , 64'b0000},
+						{64'b0010 , 64'b0000},
+						{64'b0011 , 64'b0000},
+						{64'b0100 , 64'b0000},
+						{64'b0101 , 64'b0101},
+						{64'b0110 , 64'b0110},
+						{64'b0111 , 64'b0111},
+						{64'b1000 , 64'b1000},
+						{64'b1001 , 64'b1001}};
 
 	// Instantiate device under test
 	flopr #(N) dut(clk, reset, d, q);
@@ -33,8 +32,7 @@ module flopr_tb();
 			reset = 1; qexpected = 'b0; #45; reset = 0;
 		end
 
-	
-	// apply test vectors on falling edge of clk
+	// Apply test vectors on falling edge of clk
 	always @(negedge clk)
 		begin
 			{d, qexpected} = testvectors[i];
@@ -42,7 +40,7 @@ module flopr_tb();
 	
 	 
 	// Check results on falling edge of clk
-   always @(posedge clk)
+   	always @(posedge clk)
 			begin
 			#1;
 			if (reset === '1) 
