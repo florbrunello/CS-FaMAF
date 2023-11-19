f3cb4e6066fc76c550837e1591421c71592f2cff
Florencia Brunello <florenciabrunello@mi.unc.edu.ar>
2023-11-19 11:35:59 -0300

    Update memory.sv

--- Tercero/2doCuatrimestre/AdC/Laboratorio/Laboratorio2/memory.sv
+++ Tercero/2doCuatrimestre/AdC/Laboratorio/Laboratorio2/memory.sv
@@ -1,10 +1,8 @@
-// Etapa: MEMORY
+// Memory
 
 module memory 	(input logic Branch_M, zero_M,					
 					output logic PCSrc_M);
-					
-	//MODIF para branch 
 	
-	assign PCSrc_M = (Branch_M & zero_M); //| InconBranch;
+	assign PCSrc_M = (Branch_M & zero_M);
 	
 endmodule
